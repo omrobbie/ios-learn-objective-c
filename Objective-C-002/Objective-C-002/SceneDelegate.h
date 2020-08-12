40d28690823be9133b1cdcc25df6696f8afbb7c5
omrobbie <putu.roby@yahoo.com>
2020-08-12 11:43:26 +0800

    * start objective c 002 project *

---
+++ Objective-C-002/Objective-C-002/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Objective-C-002
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
