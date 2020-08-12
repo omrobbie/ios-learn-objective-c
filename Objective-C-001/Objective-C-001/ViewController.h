56f74df65fe163173439cf54ab7756411587aa7a
omrobbie <putu.roby@yahoo.com>
2020-08-12 10:17:13 +0800

    * start objective c 001 project *

---
+++ Objective-C-001/Objective-C-001/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C-001
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Objective-C-001/Objective-C-001/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Objective-C-001
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
