9c7333c5db39579258238af81673c5847ca196f6
omrobbie <putu.roby@yahoo.com>
2020-08-12 13:44:46 +0800

    * start objective c 003 project *

---
+++ Objective-C-003/Objective-C-003/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Objective-C-003
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Objective-C-003/Objective-C-003/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Objective-C-003
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
