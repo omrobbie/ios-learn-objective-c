2a013a8904ebdcfe6e207c267b4ae30e03f17a5a
omrobbie <putu.roby@yahoo.com>
2020-08-12 14:54:18 +0800

    create data model

---
+++ Objective-C-003/Objective-C-003/Course.h
@@ -0,0 +1,16 @@
+//
+//  Course.h
+//  Objective-C-003
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Course : NSObject
+
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSNumber *numberOfLessons;
+
+@end
---
+++ Objective-C-003/Objective-C-003/Course.m
@@ -0,0 +1,13 @@
+//
+//  Course.m
+//  Objective-C-003
+//
+//  Created by omrobbie on 12/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+#import "Course.h"
+
+@implementation Course
+
+@end
