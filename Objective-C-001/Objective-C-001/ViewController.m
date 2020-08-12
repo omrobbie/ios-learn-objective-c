692cdbaa1d2810193a098e335d7f26ca49cf19f8
omrobbie <putu.roby@yahoo.com>
2020-08-12 10:58:52 +0800

    show alert controller

--- Objective-C-001/Objective-C-001/ViewController.m
+++ Objective-C-001/Objective-C-001/ViewController.m
@@ -37,6 +37,14 @@
 }
 
 - (IBAction)btnMario:(id)sender {
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Enter a height" message:@"Mario" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *actionDone = [UIAlertAction actionWithTitle:@"Done" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Done!");
+    }];
+
+    [alertVC addAction:actionDone];
+
+    [self presentViewController:alertVC animated:true completion:nil];
     [self marioWithLevels:10];
 }
 
